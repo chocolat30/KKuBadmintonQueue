50a7cac9749c26717bb4c9a89f2514940b494660
chocolat30 <porramase.00@gmail.com>
2026-06-16 00:00:54 +0700

    Fix

--- server.js
+++ server.js
@@ -57,3 +57,5 @@ process.on('unhandledRejection', (reason, promise) => {
 // Start server
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
