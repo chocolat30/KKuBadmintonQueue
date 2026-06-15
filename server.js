cdcb4fdd450e2bfcc33d23ff55d91c8de36675c5
chocolat30 <porramase.00@gmail.com>
2026-06-15 23:57:23 +0700

    Add vercel deplotment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { registerCourtHandlers } = require("./sockets/courtSockets");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:3000", "http://127.0.0.1:3000"] }
+  cors: { origin: [`http://localhost:${port}`, `http://127.0.0.1:${port}`] }
 });
 
 // Initialize Service
